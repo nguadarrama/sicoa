685f6585013e20eac2ae3b0de0277180052bdea9
Nadia <nguadarrama@anzen.com.mx>
2018-10-25 10:10:03 -0500

    Reportes Gabo

---
+++ src/main/webapp/recursos/script/reportes/reportes.js
@@ -0,0 +1,171 @@
+$(document).ready(function() {
+	
+	//pobla los campos del modal de justificación
+	$('.nBtn').on('click', function(event) { 					//botón justifica
+		event.preventDefault();
+		var href = $(this).attr('href');
+		var text = $(this).text();
+		
+		$('#fechaInicial').val($('#validBeforeDatepicker').val());
+		$('#fechaFinal').val($('#validAfterDatepicker').val());
+		
+		$.get(href, function(asistenciaJustificacion, status) {
+			var nombre = asistenciaJustificacion.asistencia.usuarioDto.nombre + ' ' + 
+			asistenciaJustificacion.asistencia.usuarioDto.apellidoPaterno + ' ' + 
+			asistenciaJustificacion.asistencia.usuarioDto.apellidoMaterno;
+			
+			$('.justificaForm #id').val(asistenciaJustificacion.asistencia.idAsistencia);
+			$('.justificaForm #puesto').val(asistenciaJustificacion.asistencia.usuarioDto.idPuesto);
+			$('.justificaForm #unidad').val(asistenciaJustificacion.asistencia.usuarioDto.nombreUnidad);
+			$('.justificaForm #cve_m_usuario').val(asistenciaJustificacion.asistencia.usuarioDto.claveUsuario);
+			$('.justificaForm #nombre').val(nombre);
+			$('.justificaForm #fechaIngreso').val(asistenciaJustificacion.asistencia.usuarioDto.fechaIngreso);
+			$('.justificaForm #RFC').val(asistenciaJustificacion.asistencia.usuarioDto.rfc);
+			$('.justificaForm #fecha').val(asistenciaJustificacion.asistencia.entrada);
+			$('.justificaForm #tipoDia').val(asistenciaJustificacion.asistencia.idTipoDia.nombre);
+			$('.justificaForm #idTipoDia').val(asistenciaJustificacion.asistencia.idTipoDia.idTipoDia);
+			$('.justificaForm #estado').val(asistenciaJustificacion.asistencia.idEstatus.estatus);
+			$('#justificacion').val(asistenciaJustificacion.asistencia.incidencia.justificacion.justificacion);
+		});
+		
+		$('.justificaForm #justificaModal').modal();
+	});
+	
+	$("#buscarText").on("keyup", function() {
+	    var value = $(this).val().toLowerCase();
+	    $("#tableHorarios tr").filter(function() {
+	      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+	    });
+	  });
+	
+	$('.filterable .btn-filter').click(function(){
+        var $panel = $(this).parents('.filterable'),
+        $filters = $panel.find('.filters input'),
+        $tbody = $panel.find('.table tbody');
+        if ($filters.prop('disabled') == true) {
+            $filters.prop('disabled', false);
+            $filters.first().focus();
+        } else {
+            $filters.val('').prop('disabled', true);
+            $tbody.find('.no-result').remove();
+            $tbody.find('tr').show();
+        }
+    });
+
+    $('.filterable .filters input').keyup(function(e){
+        /* Ignore tab key */
+        var code = e.keyCode || e.which;
+        if (code == '9') return;
+        /* Useful DOM data and selectors */
+        var $input = $(this),
+        inputContent = $input.val().toLowerCase(),
+        $panel = $input.parents('.filterable'),
+        column = $panel.find('.filters th').index($input.parents('th')),
+        $table = $panel.find('.table'),
+        $rows = $table.find('tbody tr');
+        /* Dirtiest filter function ever ;) */
+        var $filteredRows = $rows.filter(function(){
+            var value = $(this).find('td').eq(column).text().toLowerCase();
+            return value.indexOf(inputContent) === -1;
+        });
+        /* Clean previous no-result if exist */
+        $table.find('tbody .no-result').remove();
+        /* Show all rows, hide filtered ones (never do that outside of a demo ! xD) */
+        $rows.show();
+        $filteredRows.hide();
+        /* Prepend no-result row if all rows are filtered */
+        if ($filteredRows.length === $rows.length) {
+            $table.find('tbody').prepend($('<tr class="no-result text-center"><td colspan="'+ $table.find('.filters th').length +'">No encontrado</td></tr>'));
+        }
+    });
+      
+    //filtro
+    $('.filterable .filters input').keyup(function(e){
+        /* Ignore tab key */
+        var code = e.keyCode || e.which;
+        if (code == '9') return;
+        /* Useful DOM data and selectors */
+        var $input = $(this),
+        inputContent = $input.val().toLowerCase(),
+        $panel = $input.parents('.filterable'),
+        column = $panel.find('.filters th').index($input.parents('th')),
+        $table = $panel.find('.table'),
+        $rows = $table.find('tbody tr');
+        /* Dirtiest filter function ever ;) */
+        var $filteredRows = $rows.filter(function(){
+            var value = $(this).find('td').eq(column).text().toLowerCase();
+            return value.indexOf(inputContent) === -1;
+        });
+        /* Clean previous no-result if exist */
+        $table.find('tbody .no-result').remove();
+        /* Show all rows, hide filtered ones (never do that outside of a demo ! xD) */
+        $rows.show();
+        $filteredRows.hide();
+        /* Prepend no-result row if all rows are filtered */
+        if ($filteredRows.length === $rows.length) {
+            $table.find('tbody').prepend($('<tr class="no-result text-center"><td colspan="'+ $table.find('.filters th').length +'">No encontrado</td></tr>'));
+        }
+    });
+    
+    //fechas datepicker
+    $('#validBeforeDatepicker,#validAfterDatepicker, #fecha').datepicker({
+    	beforeShowDay: $.datepicker.noWeekends, //desactiva sábado y domingo del calendario
+    	dateFormat: 'yy-mm-dd'
+    });
+    
+    //validaciones para datepicker
+    $('#buscarRango').validate({ 
+        rules: { 
+            fechaInicial: { 
+                dpCompareDate: "notAfter #validAfterDatepicker"
+            },
+		    fechaFinal: { 
+		        dpCompareDate: "notBefore #validBeforeDatepicker"
+		    } 
+        },
+    	messages: {
+    		fechaInicial: 'Ingresa fecha menor ó igual a la Fecha Final',
+    		fechaFinal: 'Ingresa fecha mayor ó igual a la Fecha Inicial'
+    	}
+    			
+    });
+    
+    $("#puesto, #unidad, #cve_m_usuario, #nombre, #fechaIngreso, #RFC, #fecha, #tipoDia, #estado, #justificacion").keydown(function (e) {
+        // no permite la entrada de texto
+        e.preventDefault();
+    });
+    
+    $('#imprimir').click(function() {
+		var imprime = document.getElementById('tableAsistencias');
+		var wme = window.open("", "", "width=900, height=700");
+		wme.document.write(imprime.outerHTML);
+		wme.document.close();
+		wme.focus();
+		wme.print();
+		wme.close();
+	});
+    
+    //agrega paginador
+    $('#tableAsistencias').DataTable({
+        "scrollY": "500px",
+        "scrollCollapse": true
+     });
+    
+    //validación para datepicker, si se selecciona uno, entonces ambos deben seleccionarse
+    $('#buscaBtn').on('click', function(event) {
+    	$("#validAfterDatepicker").prop('required',false);
+    	$("#validBeforeDatepicker").prop('required',false);
+    	
+    	var existefechaInicial = $('#validBeforeDatepicker').datepicker('getDate') != null;
+    	var existefechaFinal = $('#validAfterDatepicker').datepicker('getDate') != null;
+    	
+    	if (existefechaInicial && !existefechaFinal) {
+    		$("#validAfterDatepicker").prop('required',true);
+    	} else if (existefechaFinal && !existefechaInicial) {
+    		$("#validBeforeDatepicker").prop('required',true);
+    	} else if (existefechaInicial && existefechaFinal) {
+    		$('#buscaBtn').submit();
+    	}
+    });
+	
+}); 
