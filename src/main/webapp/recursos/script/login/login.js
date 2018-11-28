0ef57a639a58dc8651c87ace45740760ea585bf7
ocardenas <ocardenas@anzen.com.mx>
2018-11-28 15:20:59 -0600

    Login: No permite caracteres especiales en el campo usuario

--- src/main/webapp/recursos/script/login/login.js
+++ src/main/webapp/recursos/script/login/login.js
@@ -63,6 +63,15 @@ var loginJS =  (function() {
 			    }
 			});		
 		};
+		
+		$('#accesoUsuario').on('keypress', function (event) {
+		    var regex = new RegExp("^[a-zA-Z0-9]+$");
+		    var key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
+		    if (!regex.test(key)) {
+		       event.preventDefault();
+		       return false;
+		    }
+		});
 
 
 		/**
