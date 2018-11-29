5c8532f50566caee007b65e7436f3c6bc14703a3
eRoblesMorales <erobles@anzen.com.mx>
2018-11-29 01:14:33 -0600

    VALIDACION DE CONTRASENIA EN LOGIN

--- src/main/webapp/recursos/script/login/login.js
+++ src/main/webapp/recursos/script/login/login.js
@@ -72,6 +72,15 @@ var loginJS =  (function() {
 		       return false;
 		    }
 		});
+		
+		$('#accesoClave').on('keypress', function (event) {
+		    var regex = new RegExp("^[a-zA-Z0-9]+$");
+		    var key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
+		    if (!regex.test(key)) {
+		       event.preventDefault();
+		       return false;
+		    }
+		});
 
 
 		/**
